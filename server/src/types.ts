b2c4ee6fb14fd6f67f73c1c40d3c70f651da5d60
prsweet <pr.sweeeet@gmail.com>
2026-05-07 17:24:49 +0530

    fix: add nonce types

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,12 +1,19 @@
-import { t, type Static } from "elysia";
+import { t, type Context, type Static } from "elysia";
 
 export const walletAuthSchema = t.Object({
   address: t.String(),
   details: t.Object({ nonce: t.String(), message: t.String() }),
-  signature: t.String()
+  signature: t.String(),
+  username: t.Optional(t.String())
 });
 
 export type walletAuthSchema = Static<typeof walletAuthSchema>;
+
+export const nonceCreateSchema = t.Object({
+  address: t.String()
+});
+
+export type nonceCreateSchema = Static<typeof nonceCreateSchema>;
 
 type responseType = {
   success: true,
@@ -27,8 +34,15 @@ export const response: overLoadResponse = (success: boolean, data: object | null
   return { success, data, error } as responseType;
 }
 
+export interface decoratedContext extends Context {
+  userId?: string
+}
+
 export const errors = {
   typeBox400: "INVALID_REQUEST",
   nonce402: "INVALID_OR_EXPIRED_NONCE",
   emailConflict409: "EMAIL_ALREADY_EXISTS",
+  unauthorized401: "UNAUTHORIZED",
+  serverError500: "INTERNAL_SERVER_ERROR",
+  bucketCreator403: "BUCKET_CREATOR_REQUIRED"
 }
